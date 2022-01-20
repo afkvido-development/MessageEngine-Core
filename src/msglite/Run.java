c8f3ee03c90dda71d03aca6f7ac5820421cc55a0
gemsvido <kid4cars@gmail.com>
2022-01-20 09:02:36 -0800

    MessageEngine LITE
    run

--- src/msglite/Run.java
+++ src/msglite/Run.java
@@ -2,21 +2,29 @@ package msglite;
 
 import msglite.resources.c;
 import msglite.txtMessage.TextMessage;
+import msglite.version.enforceVersion;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Run {
 
+    protected static final Scanner wait = new Scanner(System.in);
+
     public static void main(String[] args) {
 
-        Scanner sc1 = new Scanner(System.in);
-        String sc2;
-        TextMessage message;
 
 
         dL("", "Running debug logs");
+        enforceVersion.enforceV();
+
+
+
+        while (true) {
+            wait.nextLine();
+        }
 
 
     }
